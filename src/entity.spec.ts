7cbd5364b96372e21f94d513248ab091e18fe34a
greguz <gregolettogiacomo@gmail.com>
2019-12-31 15:42:59 +0100

    Add entity file

--- src/entity.spec.ts
+++ src/entity.spec.ts
@@ -1,6 +1,6 @@
 import test, { ExecutionContext } from 'ava'
 
-import { create, read } from './index'
+import { create, read } from './entity'
 
 interface CommitMode {
   create: boolean;
---
+++ src/entity.ts
@@ -0,0 +1,52 @@
+import {
+  Context,
+  createContext,
+  readContext,
+  assignContext,
+  commitContext,
+  connectContext,
+  deleteContext,
+  lockContext,
+  unwrapContext,
+  updateContext
+} from './context'
+import { Source, createReader } from './read'
+import { Commit, createWriter } from './write'
+
+export type Mutator<T, U, A extends any[]> = (data: T, ...args: A) => U
+
+export interface Entity<T, O> {
+  update<U, A extends any[]>(mutator: Mutator<T, U, A>, ...args: A): Entity<U, O>
+  assign<E>(object: E): Entity<T & E, O>
+  delete(): Entity<null, O>
+  commit(): Entity<T, O>,
+  unwrap(options?: O): Promise<T>
+  connect<C>(commit: Commit<C>): Entity<T, C>
+  release(): Entity<T, any>
+}
+
+function wrapContext<S, T, O> (ctx: Context<S, T, O>): Entity<T, O> {
+  return {
+    update: (mutator, ...args) => wrapContext(updateContext(lockContext(ctx), mutator, args)),
+    assign: object => wrapContext(assignContext(lockContext(ctx), object)),
+    delete: () => wrapContext(deleteContext(lockContext(ctx))),
+    commit: () => wrapContext(commitContext(lockContext(ctx))),
+    unwrap: options => unwrapContext(lockContext(ctx), options),
+    connect: commit => wrapContext(connectContext(lockContext(ctx), createWriter(commit))),
+    release: () => wrapContext(connectContext(lockContext(ctx), createWriter()))
+  }
+}
+
+export function create<T, O> (
+  source: Source<T, O>,
+  commit?: Commit<O>
+): Entity<T, O> {
+  return wrapContext(createContext(createReader(source), createWriter(commit)))
+}
+
+export function read<T, O> (
+  source: Source<T, O>,
+  commit?: Commit<O>
+): Entity<T, O> {
+  return wrapContext(readContext(createReader(source), createWriter(commit)))
+}
