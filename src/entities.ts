6fa625aa425eb03f64801d88cb0230c706216530
greguz <gregolettogiacomo@gmail.com>
2020-03-07 12:17:48 +0100

    Add stream and reduce methods

--- src/entities.ts
+++ src/entities.ts
@@ -1,7 +1,14 @@
-import { Readable, Writable, Transform, pipeline } from 'readable-stream'
+import { Readable, Writable, pipeline } from 'readable-stream'
 
 import { Commit, Entity, Mutator, create, read } from './entity'
 import { Many, getMany } from './many'
+
+export type Reducer<T, O, R> = (
+  accumulator: R,
+  entity: Entity<T, O>,
+  index: number,
+  options?: O
+) => Promise<R>
 
 export interface Entities<T, O> {
   update<U, A extends any[]> (mutator: Mutator<T, U, A>, ...args: A): Entities<U, O>
@@ -9,6 +16,8 @@ export interface Entities<T, O> {
   delete (): Entities<null, O>
   commit (): Entities<T, O>,
   unwrap (options?: O): Promise<T[]>
+  stream (options?: O): Readable
+  reduce<R> (reducer: Reducer<T, O, R>, init: R, options?: O): Promise<R>
 }
 
 interface Context<S, T, O> {
@@ -77,16 +86,6 @@ function commitContext<S, T, O> (
   return mapContext(ctx, entity => entity.commit())
 }
 
-function createEntityMapper (ctx: Context<any, any, any>): Transform {
-  return new Transform({
-    objectMode: true,
-    transform (data, encoding, callback) {
-      this.push(ctx.mapper(data))
-      callback()
-    }
-  })
-}
-
 function unwrapContext<S, T, O> (
   ctx: Context<S, T, O>,
   options?: O
@@ -95,11 +94,12 @@ function unwrapContext<S, T, O> (
     const results: T[] = []
     pipeline(
       ctx.extract(options),
-      createEntityMapper(ctx),
       new Writable({
         objectMode: true,
-        write (data: Entity<T, O>, encoding, callback) {
-          data.unwrap(options)
+        write (data: S, encoding, callback) {
+          ctx
+            .mapper(data)
+            .unwrap(options)
             .then(result => {
               results.push(result)
               callback()
@@ -112,6 +112,72 @@ function unwrapContext<S, T, O> (
           reject(error)
         } else {
           resolve(results)
+        }
+      }
+    )
+  })
+}
+
+function streamContext<S, T, O> (
+  ctx: Context<S, T, O>,
+  options?: O
+): Readable {
+  let reading = false
+  return new Readable({
+    objectMode: true,
+    read () {
+      if (reading) {
+        return
+      }
+      reading = true
+      const self = this
+      pipeline(
+        ctx.extract(options),
+        new Writable({
+          objectMode: true,
+          write (data: S, encoding, callback) {
+            self.push(ctx.mapper(data))
+            callback()
+          }
+        }),
+        error => {
+          if (error) {
+            self.emit('error', error)
+          } else {
+            self.push(null)
+          }
+        }
+      )
+    }
+  })
+}
+
+function reduceContext<S, T, O, R> (
+  ctx: Context<S, T, O>,
+  reducer: Reducer<T, O, R>,
+  accumulator: R,
+  options?: O
+): Promise<R> {
+  return new Promise<R>((resolve, reject) => {
+    let index = 0
+    pipeline(
+      ctx.extract(options),
+      new Writable({
+        objectMode: true,
+        write (data: S, encoding, callback) {
+          reducer(accumulator, ctx.mapper(data), index++, options)
+            .then(result => {
+              accumulator = result
+              callback()
+            })
+            .catch(callback)
+        }
+      }),
+      error => {
+        if (error) {
+          reject(error)
+        } else {
+          resolve(accumulator)
         }
       }
     )
@@ -132,7 +198,9 @@ function wrapContext<S, T, O> (ctx: Context<S, T, O>): Entities<T, O> {
     assign: object => wrapContext(assignContext(lockContext(ctx), object)),
     delete: () => wrapContext(deleteContext(lockContext(ctx))),
     commit: () => wrapContext(commitContext(lockContext(ctx))),
-    unwrap: options => unwrapContext(lockContext(ctx), options)
+    unwrap: options => unwrapContext(lockContext(ctx), options),
+    stream: options => streamContext(lockContext(ctx), options),
+    reduce: (reducer, init, options) => reduceContext(ctx, reducer, init, options)
   }
 }
 
