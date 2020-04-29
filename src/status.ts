27abc69f9e089e6657ac94a53c4ea295ac8c2edd
greguz <gregolettogiacomo@gmail.com>
2020-04-29 15:18:01 +0200

    Add comments

--- src/status.ts
+++ src/status.ts
@@ -2,10 +2,25 @@ import { UndefinedEntityError } from './errors'
 import { isUndefined } from './utils'
 
 export interface Status<T> {
+  /**
+   * Expresses a user's create intent.
+   */
   created: boolean
+  /**
+   * Expresses a user's update intent.
+   */
   updated: boolean
+  /**
+   * Expresses a user's delete intent.
+   */
   deleted: boolean
+  /**
+   * Represents the current entity's data present inside its data source.
+   */
   source: T | null
+  /**
+   * Represents the entity's data after all described mutations are applied.
+   */
   target: T
 }
 
