0e921e81082bfa71ae11f533724ab3e905261888
greguz <gregolettogiacomo@gmail.com>
2020-05-27 17:51:45 +0200

    Remove unused function

--- src/utils.ts
+++ src/utils.ts
@@ -8,12 +8,6 @@ export function isNull (value: any): value is null {
   return value === null
 }
 
-export function isObjectLike (value: any): value is object {
-  return typeof value === 'object' && !isNull(value)
-}
-
 export function objectify<O> (value?: O): Partial<O> {
-  return isObjectLike(value)
-    ? value
-    : {}
+  return typeof value === 'object' && !isNull(value) ? value : {}
 }
