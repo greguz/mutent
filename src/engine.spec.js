67c1ee329225f1a84452d453a8ea0d02d3774c05
greguz <gregolettogiacomo@gmail.com>
2020-11-04 16:24:10 +0100

    Add more tests

--- src/engine.spec.js
+++ src/engine.spec.js
@@ -1,57 +1,99 @@
 import test from 'ava'
 
+import Ajv from 'ajv'
+
 import { createEngine } from './engine'
 
 test('engine:defineConstructor', t => {
-  class Teapot {}
+  class Tweedledum {}
+  class Tweedledee {}
 
-  const engine = createEngine().defineConstructor('Teapot', Teapot)
+  const engine = createEngine({
+    constructors: { Tweedledum }
+  }).defineConstructor('Tweedledee', Tweedledee)
 
   const schema = engine.compile({
     type: 'object',
     properties: {
       a: {
         type: 'object',
-        instanceof: 'Date'
+        instanceof: 'Tweedledum'
       },
       b: {
         type: 'object',
-        instanceof: 'Teapot'
+        instanceof: 'Tweedledee'
+      },
+      c: {
+        type: 'object',
+        instanceof: 'Alice'
       }
     }
   })
 
   schema.validate({})
   schema.validate({
-    a: new Date(),
-    b: new Teapot()
+    a: new Tweedledum(),
+    b: new Tweedledee()
   })
-  t.throws(() => schema.validate({ a: 'nope' }), {
+  t.throws(() => schema.validate({ a: {} }), {
     code: 'EMUT_INVALID_DATA'
   })
-  t.throws(() => schema.validate({ b: 'nope' }), {
+  t.throws(() => schema.validate({ b: {} }), {
+    code: 'EMUT_INVALID_DATA'
+  })
+  t.throws(() => schema.validate({ c: {} }), {
     code: 'EMUT_INVALID_DATA'
   })
 })
 
 test('engine:defineParser', t => {
-  const engine = createEngine().defineParser('round', value =>
-    Math.round(value)
-  )
+  const engine = createEngine({
+    parsers: { ceil: Math.ceil }
+  }).defineParser('floor', Math.floor)
 
   const schema = engine.compile({
     type: 'object',
     properties: {
-      value: {
+      a: {
+        type: 'number',
+        parse: 'ceil'
+      },
+      b: {
+        type: 'number',
+        parse: 'floor'
+      },
+      c: {
         type: 'number',
         parse: 'round'
       }
     }
   })
 
-  t.deepEqual(schema.parse({ value: 42.4 }), { value: 42 })
-  t.deepEqual(schema.parse({ value: 41.5 }), { value: 42 })
-  t.throws(() => schema.parse({ value: 'nope' }), {
+  schema.parse({})
+  t.deepEqual(schema.parse({ a: 41.5 }), { a: 42 })
+  t.deepEqual(schema.parse({ b: 42.4 }), { b: 42 })
+  t.throws(() => schema.parse({ a: 'nope' }), {
     code: 'EMUT_INVALID_DATA'
   })
+  t.throws(() => schema.parse({ b: 'nope' }), {
+    code: 'EMUT_INVALID_DATA'
+  })
+  t.throws(() => schema.parse({ c: 42.1 }), {
+    code: 'EMUT_EXPECTED_PARSER'
+  })
+})
+
+test('engine:keywordsCheck', t => {
+  createEngine()
+
+  const a = new Ajv()
+  createEngine({ ajv: a })
+
+  const b = new Ajv()
+  b.addKeyword('instanceof', {})
+  t.throws(() => createEngine({ ajv: b }), { code: 'EMUT_RESERVED_KEYWORD' })
+
+  const c = new Ajv()
+  c.addKeyword('parse', {})
+  t.throws(() => createEngine({ ajv: c }), { code: 'EMUT_RESERVED_KEYWORD' })
 })
